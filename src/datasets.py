c17cc949c814fd6e1a236b5a43b5070f063ce7ca
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2025-02-27 15:20:00 -0700

    Added wine dataset

--- src/datasets.py
+++ src/datasets.py
@@ -3,9 +3,11 @@ Datasets
 """
 
 import numpy as np
+import sklearn
 import torch
 from numpy.typing import NDArray
 from torch.utils.data import Dataset
+from ucimlrepo import fetch_ucirepo
 
 np.random.seed(0)
 
@@ -37,3 +39,42 @@ class Poly(Dataset):
 
     def __getitem__(self, i: int) -> tuple[torch.Tensor, torch.Tensor]:
         return self.x[i].unsqueeze(0), self.y[i].unsqueeze(0)
+
+
+def get_wine_data() -> tuple[Dataset, Dataset]:
+    """
+    Returns training and testing datasets for the wine data
+
+    Returns:
+        tuple[Dataset, Dataset]: train, test
+    """
+
+    # Fetch dataset
+    wine_quality = fetch_ucirepo(id=186)
+    X: NDArray = wine_quality.data.features.values.astype(
+        np.float32
+    )  # samples x features
+    y: NDArray = wine_quality.data.targets.values.astype(np.float32)  # samples x 1
+
+    # Split the dataset
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
+
+    # Scale features
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    train_ds = torch.utils.data.TensorDataset(
+        torch.from_numpy(X_train_scaled), torch.from_numpy(y_train)
+    )
+    test_ds = torch.utils.data.TensorDataset(
+        torch.from_numpy(X_test_scaled), torch.from_numpy(y_test)
+    )
+
+    return train_ds, test_ds
+
+
+if __name__ == "__main__":
+    train, test = get_wine_data()
