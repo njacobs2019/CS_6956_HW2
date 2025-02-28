b530507c0a32b450bfc22513c20b6321b04dd85a
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2025-02-28 12:47:44 -0700

    feat: ensemble evaluation and MicroPoly model

--- src/ensemble.py
+++ src/ensemble.py
@@ -6,6 +6,7 @@ from typing import NamedTuple, Sequence
 
 import torch
 from torch import Tensor, nn
+from torch.utils.data import DataLoader
 
 
 class Result(NamedTuple):
@@ -49,3 +50,43 @@ def predict(x: Tensor, models: Sequence[nn.Module]) -> Result:
         max=torch.max(predictions, dim=0)[0],
         sd=torch.std(predictions, dim=0),
     )
+
+
+def evaluate(
+    loader: DataLoader, device: torch.device, ensemble: Sequence[nn.Module]
+) -> tuple[float, float]:
+    """
+    Evaluates performance and uncertainty of an ensemble
+
+    Args:
+        loader (DataLoader): Data loader
+        device (torch.device): Compute device
+        ensemble (Sequence[nn.Module]): Ensemble
+
+    Returns:
+        tuple[float, float]: MSE, average SD
+    """
+
+    mse = 0.0
+    sd = 0.0
+    total_samples = 0
+
+    with torch.no_grad():
+        for x, y in loader:
+            batch_size = x.size(0)
+            total_samples += batch_size
+
+            x = x.to(device)
+            y = y.to(device)
+            pred = predict(x, ensemble)
+
+            mse_update = torch.mean(torch.square(pred.mean - y))
+            sd_update = torch.mean(pred.sd)
+
+            mse += mse_update.item() * batch_size
+            sd += sd_update.item() * batch_size
+
+    mse = mse / total_samples
+    sd = sd / total_samples
+
+    return mse, sd
--- src/models.py
+++ src/models.py
@@ -31,3 +31,29 @@ class PolyModelSmall(nn.Module):
             Tensor: Point estimate prediction of shape (batch_size, output_dim)
         """
         return self.network(x)
+
+
+class PolyModelMicro(nn.Module):
+    """
+    Regression model for the poly dataset
+    """
+
+    def __init__(self, input_dim: int, hidden_dim: int = 64, output_dim: int = 1):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Forward pass returns a single point estimate
+
+        Args:
+            x (Tensor): Input tensor of shape (batch_size, input_dim)
+
+        Returns:
+            Tensor: Point estimate prediction of shape (batch_size, output_dim)
+        """
+        return self.network(x)
